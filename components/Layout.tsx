f344942c3c0aca021f195c0bf4738f4e751254e9
Rasha Faheem <rashafaheem@Rashas-MacBook-Pro.local>
2025-10-22 13:44:47 +0500

    Add nav bar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,18 @@
 import styles from '../styles/Layout.module.css';
 import React, { ReactNode } from "react";
-
+import Nav from './Nav';
 type LayoutProps = {
   children: ReactNode;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
+  return (<>
+  <Nav  />
     <div className={styles.container}>
         <h1>Hi</h1>
       <main className={styles.main}>{children}</main>
     </div>
-  );
+    </> );
 };
 
 export default Layout;
